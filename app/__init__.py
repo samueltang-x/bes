627494052e40567160499c9e8ab204cc1c560630
Samuel Tang <samuel.x.tang@gmail.com>
2019-05-10 23:02:34 +0800

    fix bug that debug log is alway enabled

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,4 @@
-import logging
 from flask import Flask
-
-# Default setting for logging.
-log_level = logging.DEBUG
-FORMAT = '%(asctime)-15s [bes] %(levelname)-8s %(message)s'
-logging.basicConfig(level=log_level, format=FORMAT)
 
 server = Flask(__name__)
 
--- run_https.py
+++ run_https.py
@@ -1,5 +1,17 @@
 #!venv/bin/python
 
 from app import server, config
+import logging
 
-server.run(**config.create_srv_options())
+server_options = config.create_srv_options()
+
+# Default setting for logging.
+if server_options['debug']:
+  log_level = logging.DEBUG
+else:
+  log_level = logging.INFO
+
+FORMAT = '%(asctime)-15s [bes] %(levelname)-8s %(message)s'
+logging.basicConfig(level=log_level, format=FORMAT)
+
+server.run(**server_options)
